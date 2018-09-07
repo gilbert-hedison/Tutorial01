7108379f6cbd91db5359cf9aefe80657a3a335f9
nathanael.lemuella <nathanael.lemuella@ui.ac.id>
2018-09-07 19:47:10 +0700

    Initial Commit Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,23 @@
+
+public class Mahasiswa {
+	private int npm;
+	private String nama;
+	public Mahasiswa(int npm, String nama) {
+		super();
+		this.npm = npm;
+		this.nama = nama;
+	}
+	public int getNpm() {
+		return npm;
+	}
+	public void setNpm(int npm) {
+		this.npm = npm;
+	}
+	public String getNama() {
+		return nama;
+	}
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+	
+}
