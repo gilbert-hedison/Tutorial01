6f2b76dc8bd80441e968da642e92163fef3b6415
nathanael.lemuella <nathanael.lemuella@ui.ac.id>
2018-09-07 19:48:02 +0700

    Commit Dosen.java

---
+++ Dosen.java
@@ -0,0 +1,23 @@
+
+public class Dosen {
+	private int noID;
+	private String nama;
+	public Dosen(int noID, String nama) {
+		super();
+		this.noID = noID;
+		this.nama = nama;
+	}
+	public int getNoID() {
+		return noID;
+	}
+	public void setNoID(int noID) {
+		this.noID = noID;
+	}
+	public String getNama() {
+		return nama;
+	}
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+	
+}
